3a79117685e36748eb079feec22b9c4363c14bac
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2024-02-18 00:26:07 +0100

    👷 Add monkey language tokens

---
+++ token/token.go
@@ -0,0 +1,36 @@
+package token
+
+// Rob Pike used int probably for performance reasons.
+// Thorsten explains this in chapter 1.2 of his book.
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	// Special tokens
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // Integer type
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
