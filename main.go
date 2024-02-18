ae7cf635047df07dea6c8a776b0c3b280dbd61ca
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2024-02-18 17:28:59 +0100

    ✨ Create simple REPL to print tokens

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"goparsor/repl"
+	"os"
+	"os/user"
+)
+
 func main() {
-	println("Hello, World!")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Welcome %s!\n", user.Username)
+	fmt.Printf("Type in commands and create your new funky project, you monkey!\n")
+	repl.Start(os.Stdin)
 }
