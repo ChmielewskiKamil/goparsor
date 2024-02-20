b2e3f6a311d18766054cd3e18d1196a1a97f2cb7
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2024-02-20 08:03:44 +0100

    👷 Create basic parser structure

---
+++ parser/parser.go
@@ -0,0 +1,32 @@
+package parser
+
+import (
+	"goparsor/ast"
+	"goparsor/lexer"
+	"goparsor/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	currToken token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.NextToken()
+	p.NextToken()
+
+	return p
+}
+
+func (p *Parser) NextToken() {
+	p.currToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
