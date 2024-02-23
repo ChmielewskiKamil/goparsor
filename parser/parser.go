d71ef3d16895180a0d36e04391bad69fffee5039
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2024-02-23 11:52:14 +0100

    👷 Parse let statements

--- parser/parser.go
+++ parser/parser.go
@@ -28,5 +28,73 @@ func (p *Parser) NextToken() {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for !p.currTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+
+		p.NextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.currToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	// We now that the current token is a statement
+	stmt := &ast.LetStatement{Token: p.currToken}
+
+	// The next token has to be identifier
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	// We already have the statement token (LET), now
+	// we get its name e.g., let balance = 10
+	// balance is the name
+	stmt.Name = &ast.Identifier{Token: p.currToken, Value: p.currToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// @TODO: Implement expression handling
+	// We skip the value of an expression for now.
+	for !p.currTokenIs(token.SEMICOLON) {
+		p.NextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) currTokenIs(tkn token.TokenType) bool {
+	return p.currToken.Type == tkn
+}
+
+func (p *Parser) peekTokenIs(tkn token.TokenType) bool {
+	return p.peekToken.Type == tkn
+}
+
+func (p *Parser) expectPeek(tkn token.TokenType) bool {
+	if p.peekTokenIs(tkn) {
+		// The token type is what we expected it to be, so
+		// just advance the lexer to the next token
+		p.NextToken()
+		return true
+	} else {
+		return false
+	}
+
 }
---
+++ parser/parser_test.go
@@ -0,0 +1,68 @@
+package parser
+
+import (
+	"goparsor/ast"
+	"goparsor/lexer"
+	"testing"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+    let x = 5;
+    let foo = 123;
+    
+    let bar = 0;
+    `
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+
+	if program == nil {
+		t.Fatalf("ParseProgram(...) returned nil")
+	}
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("expected 3 statements, got %d", len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"foo"},
+		{"bar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testLetStatement(t *testing.T, stmt ast.Statement, identName string) bool {
+	if stmt.TokenLiteral() != "let" {
+		t.Errorf("stmt.tokenLiteral not 'let', got: %q", stmt.TokenLiteral())
+		return false
+	}
+
+	// Assert the type of the stmt to be of type ast.LetStatement
+	letStmt, ok := stmt.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("s not *ast.LetStatement. got=%T", stmt)
+		return false
+	}
+	if letStmt.Name.Value != identName {
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s", identName, letStmt.Name.Value)
+		return false
+	}
+	if letStmt.Name.TokenLiteral() != identName {
+		t.Errorf("s.Name not '%s'. got=%s", identName, letStmt.Name)
+		return false
+	}
+
+	return true
+}
